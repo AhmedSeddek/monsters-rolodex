7310c6c5c6f63af1a3fc680ddbcb376030904520
ahmed seddek <ahmedonline123@hotmail.com>
2020-07-15 22:41:53 +0200

    Updates

---
+++ precache-manifest.fe96308e6e0cefaf0a43b7c0bc572d96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475c38159905c492b25945b69800a6f7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d3bfdb05b27fc3808593",
+    "url": "/monsters-rolodex/static/css/main.9a4ecffb.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bfdb05b27fc3808593",
+    "url": "/monsters-rolodex/static/js/main.6daab861.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
